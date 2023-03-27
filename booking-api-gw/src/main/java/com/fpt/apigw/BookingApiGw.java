7b3f072a17541456bf4fb3432a2b0b1c791b6295
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2023-03-27 17:58:19 +0700

    update dependencies - remove deprecated sleuth - remove eureka annotations

--- booking-api-gw/src/main/java/com/fpt/apigw/BookingApiGw.java
+++ booking-api-gw/src/main/java/com/fpt/apigw/BookingApiGw.java
@@ -2,10 +2,8 @@ package com.fpt.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class BookingApiGw {
     public static void main(String[] args) {
         SpringApplication.run(BookingApiGw.class, args);
--- booking-user-service/src/main/java/com/fpt/user/UserServiceApplication.java
+++ booking-user-service/src/main/java/com/fpt/user/UserServiceApplication.java
@@ -1,15 +1,11 @@
 package com.fpt.user;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
-
 }
