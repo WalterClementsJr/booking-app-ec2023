2e7b6c1fd494fe6d39109c84772686e4fcf82cb2
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2023-03-29 16:06:58 +0700

    setup grafana + prometheus containers + Spring Boot dependencies

--- sonar-scan.cmd
+++ sonar-scan.cmd
@@ -1,1 +1,1 @@
-./mvnw sonar:sonar -Dsonar.projectKey=booking -Dsonar.host.url=http://127.0.0.1:9001 -Dsonar.login=admin -Dsonar.password=password
+./mvnw sonar:sonar -Dsonar.projectKey=WalterClementsJr_booking-app-ec2023 -Dsonar.host.url=http://127.0.0.1:9001 -Dsonar.login=admin -Dsonar.password=password
