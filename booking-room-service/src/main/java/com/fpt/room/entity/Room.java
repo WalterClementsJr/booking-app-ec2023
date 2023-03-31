45729f65f0db1c9563520442480f3e3cf678ad02
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2023-03-31 11:28:30 +0700

    entities

---
+++ booking-room-service/src/main/java/com/fpt/room/entity/Room.java
@@ -0,0 +1,34 @@
+package com.fpt.room.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "room")
+@NoArgsConstructor
+public class Room extends BaseEntity {
+    @Column(name = "room_number")
+    private Integer roomNumber;
+    @Column(name = "status")
+    private String status;
+    @Column(name = "amenities")
+    private String amenities;
+    @Column(name = "image")
+    private String image;
+    @OneToOne
+    private RoomType roomType;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+}
---
+++ booking-room-service/src/main/java/com/fpt/room/entity/RoomType.java
@@ -0,0 +1,32 @@
+package com.fpt.room.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "room_type")
+@NoArgsConstructor
+public class RoomType extends BaseEntity {
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "maxOccupancy")
+    private Integer maxOccupancy;
+    @Column(name = "base_price")
+    private Double basePrice;
+}
